e14644f535f818c75e6459b3cfa86e9ca9b79b2a
snehilagrahari <sag21062001@outlook.com>
2022-11-10 09:00:36 +0530

    COMBINED FILES

--- Scripts/src/index.js
+++ Scripts/src/index.js
@@ -3,5 +3,11 @@ document.getElementById("mail-btn").onclick = ()=>{
     location.assign("signup.html");
 }
 document.getElementById("go_to_dash").onclick = () =>{
-    location.assign("signup.html");
+    location.assign("login.html");
+}
+document.getElementById("login_btn").onclick = () => {
+    location.assign("login.html")
+}
+document.getElementById("start_free_btn").onclick = () => {
+    location.assign("signup.html")
 }
